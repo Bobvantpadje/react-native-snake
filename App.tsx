aa5387093437d809fdf9a3c8beb9d01c3676c704
bobvantpadje <bobvantpadje@gmail.com>
2020-10-03 16:44:18 +0200

    created home screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Welcome to snake</Text>
+      <Text style={styles.description}>
+        This is my first react native project. You can start your game by
+        clicking the button below!
+      </Text>
+      <TouchableHighlight
+        onPress={() => {
+          alert("hihaa");
+        }}
+        accessibilityLabel="Learn more about this purple button"
+      >
+        <View style={styles.button}>
+          <Text>Start game</Text>
+        </View>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -14,8 +33,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#95B104",
+  },
+  title: {
+    fontSize: 30,
+    textAlign: "center",
+    marginTop: 64,
+    fontWeight: "bold",
+  },
+  description: {
+    marginTop: 16,
+    fontSize: 20,
+    padding: 16,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "white",
+    width: "50%",
+    height: 50,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: 'center',
+    marginTop: 32
   },
 });
